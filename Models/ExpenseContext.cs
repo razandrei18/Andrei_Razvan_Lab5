bec39a465608cec478e10700e6a1f91a2357a4de
razandrei18 <razvanioan.andrei22@gmail.com>
2020-11-04 20:09:02 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Andrei_Razvan_Lab5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+        public DbSet<Expense> Expense { get; set; }
+    }
+}
